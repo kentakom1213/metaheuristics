e728b1042c744a34dba0a38497e7d975997e9b86
kentakom1213 <kentakom1213@gmail.com>
2024-05-01 00:32:09 +0900

    [add] ビジュアライザにオプティマイザを渡す

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 use eframe::egui::{self, CentralPanel, Visuals};
 use egui_plotter::EguiBackend;
-use metaheuristics::problem::benchmark::ackley::Ackley1d;
-use plotters::prelude::*;
+use metaheuristics::{
+    opt::{algorithms::hill_climbing::HillClimbingAlgorithm, optimize::Optimizer},
+    problem::{benchmark::ackley::Ackley1d, problem::Problem},
+};
+use std::marker::PhantomData;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Metaheuristics Visualizer",
         native_options,
-        Box::new(|cc| Box::new(Simple::new(cc))),
+        Box::new(|cc| {
+            Box::new(Visualizer::new(
+                cc,
+                HillClimbingAlgorithm::new(Ackley1d, 1.0),
+            ))
+        }),
     )
     .unwrap();
 }
 
-struct Simple;
+struct Visualizer<P: Problem, Opt: Optimizer<P>> {
+    /// 最適化用のインスタンス
+    optimizer: Opt,
+    phantom: PhantomData<P>,
+}
 
-impl Simple {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+impl<P: Problem, Opt: Optimizer<P>> Visualizer<P, Opt> {
+    fn new(cc: &eframe::CreationContext<'_>, optimizer: Opt) -> Self {
         // Disable feathering as it causes artifacts
         let context = &cc.egui_ctx;
 
@@ -27,11 +39,14 @@ impl Simple {
         // Also enable light mode
         context.set_visuals(Visuals::light());
 
-        Self
+        Self {
+            optimizer,
+            phantom: PhantomData::<P>,
+        }
     }
 }
 
-impl eframe::App for Simple {
+impl<P: Problem, Opt: Optimizer<P>> eframe::App for Visualizer<P, Opt> {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
             // 描画エリア
